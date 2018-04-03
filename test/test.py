5e10e97f9c9c23b71f2175bfaf05e73208580d15
Vahen <naeos@laposte.net>
2018-04-03 20:07:55 +0200

    - Mise en forme

--- test/test.py
+++ test/test.py
@@ -6,35 +6,32 @@ from scipy.signal import *
 from scipy.ndimage import *
 import scipy
 
-img = cv2.imread("./humanChienFondSeuil.png", 0)
-img2 = cv2.imread("./salesman2.png", 0)
-img2 = cv2.bitwise_not(img2)
+
+# Pure bullshit
+# def testMaxLoc(som, l):
+#     maximum = []
+#     somClean = [x for x in som if x > 0]
+#     size = len(somClean)
+#     print(somClean)
+#     offset = 0
+#     for i in range(len(som)):
+#         if som[i] != 0:
+#             offset = i
+#             break
+#
+#     for i in range(size):
+#         max = 0
+#         indice = 0
+#         if i-l > 0 and i+l < size:
+#             for j in range(i - l, i + l):
+#                 if somClean[j] > max:
+#                     max = somClean[j]
+#                     indice = j
+#         maximum.append(indice+offset)
+#     return maximum
 
 
-def testMaxLoc(som, l):
-    maximum = []
-    somClean = [x for x in som if x > 0]
-    size = len(somClean)
-    print(somClean)
-    offset = 0
-    for i in range(len(som)):
-        if som[i] != 0:
-            offset = i
-            break
-
-    for i in range(size):
-        max = 0
-        indice = 0
-        if i-l > 0 and i+l < size:
-            for j in range(i - l, i + l):
-                if somClean[j] > max:
-                    max = somClean[j]
-                    indice = j
-        maximum.append(indice+offset)
-    return maximum
-
-
-def maxLoc(img):
+def isPerson(img, sigma):
     som = []
     h, w = img.shape
     for i in range(w):
@@ -42,48 +39,14 @@ def maxLoc(img):
         for j in range(h):
             tmpSum += img[j][i]
         som.append(tmpSum)
-    # todo faire un max local avec seuil minimum
-    print(som)
 
     x = np.array(range(w))
-    filtered = filters.gaussian_filter1d(som,3)
-    print(filtered)
+    filtered = filters.gaussian_filter1d(som, sigma)
     y = np.array(filtered)
 
-    # maxm = argrelextrema(y, np.greater)
-    # maxm = argrelmax(y)
-    maxm = testMaxLoc(filtered,10)
-
+    maxm = argrelmax(y)  # maxm = argrelextrema(y, np.greater)
     minm = argrelextrema(y, np.less)
-    print(maxm)
-    print(minm)
 
-    # Supprime la gauche et ne conserve que le pic central
-    # index = 0
-    # for i in range(len(som)):
-    #     if som[i] != 0:
-    #         index = i
-    #         break
-    # for i in range(index, minm[0][0]):
-    #     som[i] = 0
-    #
-    # # Supprime la droite
-    # index2 = 0
-    # for i in range(len(som) - 1, 0, -1):
-    #     if som[i] != 0:
-    #         index2 = i
-    #         break
-    # for i in range(index2, minm[0][len(minm[0]) - 1],-1):
-    #     som[i] = 0
-    #
-    # print (index)
-    #
-    # print(index2)
-    # sizePers = index2 - index
-    # print("size :"+str(sizePers))
-    # print(str(sizePers*100/w) + "%")
-
-    # y = np.array(som)
     plt.plot(x, y)
 
     for i in maxm:
@@ -94,12 +57,22 @@ def maxLoc(img):
         #          xy=(i, som[i]),
         #          arrowprops={'facecolor': 'black', 'shrink': 0.05})
     plt.show()  # affiche la figure a l'ecran
+    isMiddleMax = filtered[maxm[0][1]] > filtered[maxm[0][0]] and filtered[maxm[0][1]] > filtered[maxm[0][2]]
+    return len(maxm[0]) == 3 and isMiddleMax
 
 
-# maxLoc(img)
+img = cv2.imread("./personne.png", 0)
+img3 = cv2.imread("./personne2.png", 0)
+img2 = cv2.imread("./salesman2.png", 0)
+img2 = cv2.bitwise_not(img2)
+print(isPerson(img, 3))
+print(isPerson(img3, 3))
 print("//////////////////////////////////////////")
-maxLoc(img2)
+print(isPerson(img2, 3))
 
-# cv2.imshow('image', img)
+# cv2.imshow('image', img3)
+# cv2.waitKey(0)
+# img3 = cv2.GaussianBlur(img,(5,5),1)
+# cv2.imshow('image', img3)
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
