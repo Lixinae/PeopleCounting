aab9a9317ebceb9ba173461831073db1f0a68c35
cdluznie <cdluznie@etud.u-pem.fr>
2018-04-14 19:26:12 +0200

    version propre et ajout des fonctions de filtrage haut-niveau

--- main.py
+++ main.py
@@ -2,58 +2,61 @@ from vxl import VxlCameraInfo, VxlVideo
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-
-from skimage import data
-from skimage.morphology import disk
-from scipy.signal import *
-from scipy.ndimage import *
+from scipy.ndimage import filters
+from numpy.linalg import norm
 
 def amplitude_objects(frame, amplitude_min, amplitude_max, openingKernelSize=5):
-    foreground = abs(frame.amplitude - background.amplitude)
-    _, objects = cv2.threshold(foreground.astype(np.uint8), 0, amplitude_max - amplitude_min,
-                               cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-    objects = cv2.morphologyEx(objects, cv2.MORPH_OPEN, np.ones((openingKernelSize, openingKernelSize), np.uint8))
-    return objects
-
+	foreground = abs(frame.amplitude - background.amplitude)
+	_,objects = cv2.threshold(foreground.astype(np.uint8), 0, amplitude_max - amplitude_min, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+	objects = cv2.morphologyEx(objects, cv2.MORPH_OPEN, np.ones((openingKernelSize, openingKernelSize),np.uint8))
+	return objects
 
 def depth_objects(frame, depth_min, depth_max, channels=256, blurKernelSize=5, openingKernelSize=5, thresholdCoeff=4.5):
-    image = ((channels - 1) * ((frame.depth - depth_min) / (depth_max - depth_min))).astype(np.uint8)
-    image = cv2.medianBlur(image, blurKernelSize)
-    bg = ((channels - 1) * ((background.depth - depth_min) / (depth_max - depth_min))).astype(np.uint8)
-    bg = cv2.medianBlur(bg, blurKernelSize)
-    foreground = bg.astype(np.int16) - image.astype(np.int16)
-    foreground = cv2.medianBlur(foreground, blurKernelSize)
-    objects = (foreground > (thresholdCoeff / 100) * channels).astype(np.uint8)
-    objects = cv2.morphologyEx(objects, cv2.MORPH_OPEN, np.ones((openingKernelSize, openingKernelSize), np.uint8))
-    return objects
-
+	image = ((channels - 1)*((frame.depth - depth_min)/(depth_max - depth_min))).astype(np.uint8)
+	image = cv2.medianBlur(image, blurKernelSize)	
+	bg = ((channels - 1)*((background.depth - depth_min)/(depth_max - depth_min))).astype(np.uint8)
+	bg = cv2.medianBlur(bg, blurKernelSize)
+	foreground = bg.astype(np.int16) - image.astype(np.int16)
+	foreground = cv2.medianBlur(foreground, blurKernelSize)
+	objects = (foreground > (thresholdCoeff/100)*channels).astype(np.uint8)
+	objects = cv2.morphologyEx(objects, cv2.MORPH_OPEN, np.ones((openingKernelSize,openingKernelSize),np.uint8))
+	return objects
 
 def binary_image(frame, amplitude_min, amplitude_max, depth_min, depth_max, channels=256, kernelSize=5):
-    amplitude = amplitude_objects(frame, amplitude_min, amplitude_max, openingKernelSize=kernelSize)
-    depth = depth_objects(frame, depth_min, depth_max, channels=channels, blurKernelSize=kernelSize,
-                          openingKernelSize=kernelSize)
-    return np.logical_or(amplitude, depth)
+	amplitude = amplitude_objects(frame, amplitude_min, amplitude_max, openingKernelSize=kernelSize)
+	depth = depth_objects(frame, depth_min, depth_max, channels=channels, blurKernelSize=kernelSize, openingKernelSize=kernelSize)
+	return np.logical_or(amplitude, depth)
 
+def compute_feature_points(vertical_sum):
+	return [
+		np.array([x,vertical_sum[x]]) for x in range(0,len(vertical_sum),5)
+	]
 
-# from scipy.signal import *
-from scipy.ndimage import filters
+def compute_shape_descriptors(binary_frame, sigma=1.5, threshold=0.):
+	height,width = binary_frame.shape
+	vertical_sum = binary_frame.sum(axis=0)
+	vertical_sum = filters.gaussian_filter1d(vertical_sum, (sigma/100)*width)
+	descriptors = []
+	current_descriptor = []
+	for value in vertical_sum:
+		if value > threshold:
+			current_descriptor.append(value)
+		elif current_descriptor != []:
+			descriptors.append(compute_feature_points(current_descriptor))
+			current_descriptor = []
+	return vertical_sum, descriptors
 
+def is_people(shape_descriptor, minRatio=0.45, maxRatio=0.55):
+	n = len(shape_descriptor)
+	if n < 2:
+		return False
+	width = norm(shape_descriptor[-1]-shape_descriptor[0])
+	height = shape_descriptor[n//2][0]
+	ratio = height/width
+	return (minRatio <= ratio <= maxRatio)
 
-def testsum(binary_frame, sigma=3.0):
-    vertical_sum = binary_frame.sum(axis=0)
-    vertical_sum = filters.gaussian_filter1d(vertical_sum, sigma)
-    return vertical_sum
-
-
-def count_people(frame, amplitude_min, amplitude_max, depth_min, depth_max):
-    """objects = binary_image(frame, amplitude_min, amplitude_max, depth_min, depth_max)
-
-
-    sigma = 3.0
-    vertical_sum = objects.sum(axis=0)
-    vertical_sum = filters.gaussian_filter1d(vertical_sum, sigma)"""
-
-    return 0
+def count_people(shape_descriptors):
+	return len([shape_descriptor for shape_descriptor in shape_descriptors if is_people(shape_descriptor)])
 
 
 cameraInfo = VxlCameraInfo('OPT8241')
@@ -64,100 +67,27 @@ video = VxlVideo.read("videos/video_1.vxl", cameraInfo)
 
 amplitude_min = min(min([frame.amplitude.min() for frame in video]), background.amplitude.min())
 amplitude_max = max(max([frame.amplitude.max() for frame in video]), background.amplitude.max())
-depth_min = min(min([frame.depth.min() for frame in video]), background.depth.min())
-depth_max = max(max([frame.depth.max() for frame in video]), background.depth.max())
+depth_min = min(min([frame.depth.min() for frame in video]),background.depth.min())
+depth_max = max(max([frame.depth.max() for frame in video]),background.depth.max())
 
 for frame in video:
-
-    binary_frame = binary_image(frame, amplitude_min, amplitude_max, depth_min, depth_max)
-
-    plt.figure(1)
-    plt.clf()
-    plt.imshow(binary_frame, cmap=plt.cm.Greys_r)
-    # plt.imshow(frame.amplitude, cmap=plt.cm.Greys_r)
-    # plt.imshow(frame.depth, cmap=plt.cm.Greys_r)
-    nb_people = count_people(frame, amplitude_min, amplitude_max, depth_min, depth_max)
-    plt.text(15, 23, 'people : ' + str(nb_people), bbox={'facecolor': 'white', 'alpha': 0.5, 'pad': 10})
-    plt.axis('off')
-
-    plt.figure(2)
-    plt.clf()
-    som = testsum(binary_frame)
-    y = np.array(som)
-    maxm = argrelmax(y)
-    plt.plot(som)
-    for i in maxm:
-        plt.plot(i, som[i], 'o', color="red")
-
-    plt.pause(0.001)
-
-plt.show()
-
-i = 10
-i = 150
-plt.clf()
-plt.imshow(process(video.avgFrame(i, 1)), cmap=plt.cm.Greys_r)
-# plt.imshow(video.avgFrame(i,1), cmap=plt.cm.Greys_r)
-nb_people = "..."
-plt.text(15, 23, 'people : ' + str(nb_people), bbox={'facecolor': 'white', 'alpha': 0.5, 'pad': 10})
-plt.axis('off')
-plt.pause(0.001)
-plt.show()
-
-"""
-for frame in video:
+	
+	binary_frame = binary_image(frame, amplitude_min, amplitude_max, depth_min, depth_max)
+	vertical_sum,shape_descriptors = compute_shape_descriptors(binary_frame)
+	nb_people = count_people(shape_descriptors)
+	
+	plt.figure(1)
 	plt.clf()
-	plt.imshow(frame.depth, cmap=plt.cm.Greys_r)
+	plt.imshow(binary_frame, cmap=plt.cm.Greys_r)
+	#plt.imshow(frame.amplitude, cmap=plt.cm.Greys_r)
+	#plt.imshow(frame.depth, cmap=plt.cm.Greys_r)
+	plt.text(15, 23, 'people : ' + str(nb_people), bbox={'facecolor':'white', 'alpha':0.5, 'pad':10})
+	plt.axis('off')
+	
+	plt.figure(2)
+	plt.clf()
+	plt.xticks(range(0,350,40))
+	plt.yticks(range(0,300,40))
+	plt.plot(vertical_sum)
+	
 	plt.pause(0.001)
-plt.show()
-"""
-
-# foreground = abs(cv2.GaussianBlur(image1,(5,5),0) - cv2.GaussianBlur(background,(5,5),0))
-# foreground = abs(image1 - background)
-# foreground = cv2.GaussianBlur(abs(image1 - background),(5,5),0)
-# foreground = cv2.GaussianBlur(abs(cv2.GaussianBlur(image1,(5,5),0) - cv2.GaussianBlur(background,(5,5),0)),(5,5),0)
-
-
-foreground = cv2.GaussianBlur(background, (5, 5), 0) - cv2.GaussianBlur(image1, (5, 5), 0)
-foreground = cv2.GaussianBlur(foreground, (5, 5), 0)
-# foreground = cv2.GaussianBlur(np.maximum(foreground,0),(5,5),0)
-
-
-"""
-radius = 15
-selem = disk(radius)
-local_otsu = rank.otsu(img, selem)
-"""
-
-mini = foreground.min()
-maxi = foreground.max()
-forg = (255 * ((foreground - mini) / (maxi - mini))).astype(np.uint8)
-
-_, objects = cv2.threshold(forg, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-
-# objects = cv2.adaptiveThreshold(forg, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 25, 1)
-
-
-# objects = (foreground > 0).astype(np.uint8)
-
-# bjects = (cv2.GaussianBlur(image1,(5,5),0)  < cv2.GaussianBlur(background,(5,5),0) - 0.1).astype(np.uint8)
-
-# objects = cv2.morphologyEx(objects, cv2.MORPH_OPEN, np.ones((5,5),np.uint8))
-# objects = cv2.morphologyEx(objects, cv2.MORPH_CLOSE, np.ones((5,5),np.uint8))
-
-"""
-plt.imshow(background, cmap=plt.cm.Greys_r)
-plt.show()
-
-plt.imshow(image1, cmap=plt.cm.Greys_r)
-plt.show()
-
-plt.imshow(foreground, cmap=plt.cm.Greys_r)
-plt.show()
-
-plt.imshow(forg, cmap=plt.cm.Greys_r)
-plt.show()
-
-plt.imshow(objects, cmap=plt.cm.Greys_r)
-plt.show()
-"""
--- vxl.py
+++ vxl.py
@@ -1,96 +1,70 @@
 import numpy as np
 import Voxel
 
-
 class VxlCameraInfo:
-    deviceResolution = {
-        'OPT8320': (60, 80),
-        'OPT8241': (240, 320)
-    }
-
-    def __init__(self, device):
-        self.system = Voxel.CameraSystem()
-        self.resolution = VxlCameraInfo.deviceResolution[device]
-
+		
+		deviceResolution = {
+			'OPT8320' : (60,80),
+			'OPT8241' : (240,320)
+		}
+		
+		def __init__(self, device):
+			self.system = Voxel.CameraSystem()
+			self.resolution = VxlCameraInfo.deviceResolution[device]
 
 class VxlFrame:
 
-    def __init__(self, attributes):
-        self.amplitude = attributes["amplitude"]
-        self.depth = attributes["depth"]
-        self.shape = self.amplitude.shape
-
+	def __init__(self, attributes):
+		self.amplitude = attributes["amplitude"]
+		self.depth = attributes["depth"]
+		self.shape = self.amplitude.shape
 
 class VxlVideo:
 
-    def __init__(self, frames):
-        self.frames = frames
+	def __init__(self, frames):
+		self.frames = frames
 
-    def __iter__(self):
-        return iter(self.frames)
+	def __iter__(self):
+		return iter(self.frames)
 
-    def __getitem__(self, index):
-        return self.frames[index]
+	def __getitem__(self, index):
+		return self.frames[index]
+		
+	def __len__(self):
+		return len(self.frames)
 
-    def __len__(self):
-        return len(self.frames)
-
-    def avgFrame(self, index, n):
-        begin = max(0, index - n)
-        end = min(len(self.frames) - 1, index + n)
-        size = end - begin + 1
-        depth = np.zeros(self.frames[begin].shape)
-        amplitude = np.zeros(self.frames[begin].shape)
-        for i in range(begin, end + 1):
-            amplitude += self.frames[i].amplitude
-            depth += self.frames[i].depth
-        amplitude = amplitude / size
-        depth = depth / size
-        return VxlFrame({
-            "amplitude": amplitude,
-            "depth": depth
-        })
-
-    @staticmethod
-    def read(vxlFile, cameraInfo):
-        reader = Voxel.FrameStreamReader(vxlFile, cameraInfo.system)  ###todo check warning
-        if not reader.isStreamGood():
-            raise ValueError("Stream is not good: " + vxlFile)
-        frames = list()
-        for i in range(reader.size()):
-            if not reader.readNext():  ###todo check warning
-                raise ValueError("Failed to read frame number " + str(i))
-            """
-            other:
-            Voxel.ToF1608Frame
-            Voxel.RawDataFrame
-            Voxel.DepthFrame
-            Voxel.PointCloudFrame
-            Voxel.XYZIPointCloudFrame
-            """
-            rawProcessedFrame = Voxel.ToF1608Frame.typeCast(reader.frames[Voxel.DepthCamera.FRAME_RAW_FRAME_PROCESSED])
-            depthFrame = Voxel.DepthFrame.typeCast(reader.frames[Voxel.DepthCamera.FRAME_DEPTH_FRAME])
-            attributes = {
-                "ambient": np.array(rawProcessedFrame._ambient, copy=True).reshape(cameraInfo.resolution),
-                "amplitude": np.array(rawProcessedFrame._amplitude, copy=True).reshape(cameraInfo.resolution),
-                "phase": np.array(rawProcessedFrame._phase, copy=True).reshape(cameraInfo.resolution),
-                "depth": np.array(depthFrame.depth, copy=True).reshape(cameraInfo.resolution)
-            }
-            frames.append(VxlFrame(attributes))
-        reader.close()
-        return VxlVideo(frames)
-
-    @staticmethod
-    def readAsAvgImage(vxlFile, cameraInfo):
-        video = VxlVideo.read(vxlFile, cameraInfo)
-        depth = np.zeros(cameraInfo.resolution)
-        amplitude = np.zeros(cameraInfo.resolution)
-        for frame in video:
-            amplitude += frame.amplitude
-            depth += frame.depth
-        amplitude = amplitude / len(video)
-        depth = depth / len(video)
-        return VxlFrame({
-            "amplitude": amplitude,
-            "depth": depth
-        })
+	@staticmethod
+	def read(vxlFile, cameraInfo):
+		reader = Voxel.FrameStreamReader(vxlFile, cameraInfo.system) ###todo check warning
+		if not reader.isStreamGood():
+			raise ValueError("Stream is not good: " + vxlFile)
+		frames = list()
+		for i in range(reader.size()):
+			if not reader.readNext(): ###todo check warning
+				raise ValueError("Failed to read frame number " +  str(i))
+			rawProcessedFrame = Voxel.ToF1608Frame.typeCast(reader.frames[Voxel.DepthCamera.FRAME_RAW_FRAME_PROCESSED])
+			depthFrame = Voxel.DepthFrame.typeCast(reader.frames[Voxel.DepthCamera.FRAME_DEPTH_FRAME])
+			attributes = {
+				"ambient" : np.array(rawProcessedFrame._ambient, copy=True).reshape(cameraInfo.resolution),
+				"amplitude" : np.array(rawProcessedFrame._amplitude, copy=True).reshape(cameraInfo.resolution),
+				"phase" : np.array(rawProcessedFrame._phase, copy=True).reshape(cameraInfo.resolution),
+				"depth" : np.array(depthFrame.depth, copy=True).reshape(cameraInfo.resolution)
+			}
+			frames.append(VxlFrame(attributes))
+		reader.close()
+		return VxlVideo(frames)
+		
+	@staticmethod
+	def readAsAvgImage(vxlFile, cameraInfo):
+		video = VxlVideo.read(vxlFile, cameraInfo)
+		depth = np.zeros(cameraInfo.resolution)
+		amplitude = np.zeros(cameraInfo.resolution)
+		for frame in video:
+			amplitude += frame.amplitude
+			depth += frame.depth
+		amplitude = amplitude/len(video)
+		depth = depth/len(video)
+		return VxlFrame({
+			"amplitude" : amplitude,
+			"depth" : depth
+		})
